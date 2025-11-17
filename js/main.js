4635103ca380b71d83ef410747356f471540d1e9
Gayan Jeewantha <gayanjeewantha100@gmail.com>
2025-11-17 21:30:21 +0530

    minor changes

--- js/main.js
+++ js/main.js
@@ -131,7 +131,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   QuoteApi.getQuote().then((q) => {
-    console.log(q);
+    // console.log(q);
     quoteEle.innerText = q;
   });
 });
