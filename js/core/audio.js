3d2ba93cf8657eb5fb8438b0fc26dbcbd34bef1b
Gayan Jeewantha <gayanjeewantha100@gmail.com>
2025-11-17 23:27:06 +0530

    fixed audio paths not working on github pages

--- js/core/audio.js
+++ js/core/audio.js
@@ -1,28 +1,28 @@
 export const audio = {
     backgroundMusic: new Howl({
-        src: "./../../audio/background_music.mp3",
+        src: "audio/background_music.mp3",
         loop: true
     }),
     enemyShoot: new Howl({
-        src: "./../../audio/enemy_shoot.mp3"
+        src: "audio/enemy_shoot.mp3"
     }),
     enemyHit: new Howl({
-        src: "./../../audio/enemy_hit.mp3"
+        src: "audio/enemy_hit.mp3"
     }),
     gameOver: new Howl({
-        src: "./../../audio/game_over.wav"
+        src: "audio/game_over.wav"
     }),
     shoot: new Howl({
-        src: "./../../audio/shoot.wav"
+        src: "audio/shoot.wav"
     }),
     start: new Howl({
-        src: "./../../audio/start.mp3"
+        src: "audio/start.mp3"
     }),
     powerup: new Howl({
-        src: "./../../audio/powerup.mp3"
+        src: "audio/powerup.mp3"
     }),
     shoot_machine_gun: new Howl({
-        src: "./../../audio/shoot_machine_gun.mp3",
+        src: "audio/shoot_machine_gun.mp3",
         volume: 0.5,
     }),
 }
